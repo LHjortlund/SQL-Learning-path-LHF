851476efb19d4a0959380f45f7cf116feee623ec
Lyanne Hjortlund-Fernandez <119303491+LHjortlund@users.noreply.github.com>
2025-10-29 14:08:42 +0100

    Add files via upload

---
+++ CTEExamples.sql
@@ -0,0 +1,27 @@
+-- CTEs
+
+WITH CTE_Example AS 
+(
+select gender, avg(salary) avg_sal, max(salary) max_sal, min(salary) min_sal, count(salary) count_sal
+from employee_demographics dem
+join employee_salary sal
+	on dem.employee_id = sal.employee_id
+group by gender
+)
+select * 
+from CTE_Example
+;
+
+-- avg of wage
+
+WITH CTE_Example AS 
+(
+select gender, avg(salary) avg_sal, max(salary) max_sal, min(salary) min_sal, count(salary) count_sal
+from employee_demographics dem
+join employee_salary sal
+	on dem.employee_id = sal.employee_id
+group by gender
+)
+select avg(avg_sal)
+from CTE_Example
+;
