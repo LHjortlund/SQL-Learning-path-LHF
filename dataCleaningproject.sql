48aa588dd51720a0813d40200d2a6fbeca2cf7dd
Lyanne Hjortlund-Fernandez <119303491+LHjortlund@users.noreply.github.com>
2026-01-22 09:42:44 +0100

    Add files via upload

--- dataCleaningproject.sql
+++ dataCleaningproject.sql
@@ -73,7 +73,126 @@ select *
 from layoffs_staging2
 where row_num > 1;
 
+SET SQL_SAFE_UPDATES = 0;
 #nu sletter vi dubletter
 delete
 from layoffs_staging2
 where row_num > 1;
+
+select *
+from layoffs_staging2;
+
+-- Standardizing data
+#trim for at fjerne mellemrum
+
+SELECT company, TRIM(company)
+FROM layoffs_staging2;
+
+update layoffs_staging2
+set company = trim(company);
+
+#nu kigger vi på industry
+SELECT distinct industry
+FROM layoffs_staging2
+order by 1;
+
+#kigger på location
+select distinct location
+from layoffs_staging2
+order by 1;
+
+#look at country
+select distinct country
+from layoffs_staging2
+order by 1; 
+#her er der to united states hvoraf den ene er med punktum
+
+select country
+from layoffs_staging2
+where country like 'United States%'
+order by 1;
+
+select distinct country, trim(trailing '.' from country)
+from layoffs_staging2
+order by 1;
+
+UPDATE layoffs_staging2
+SET country = TRIM(TRAILING '.' FROM country)
+WHERE country LIKE 'United States%';
+
+#looking at date i stedet for text til date?
+select `date`
+from layoffs_staging2;
+
+select `date`,
+str_to_date(`date`, '%m/%d/%Y')
+from layoffs_staging2;
+
+UPDATE layoffs_staging2
+SET `date` = STR_TO_DATE(`date`, '%m/%d/%Y');
+
+ALTER TABLE layoffs_staging2
+MODIFY COLUMN `date` DATE;
+
+select *
+from layoffs_staging2;
+
+-- step 3. fjerne null og blank
+
+select *
+from layoffs_staging2
+where total_laid_off is null;
+
+select *
+from layoffs_staging2
+where total_laid_off is null
+and percentage_laid_off is null;
+
+select distinct industry
+from layoffs_staging2;
+
+select *
+from layoffs_staging2
+where industry is null
+or industry = '';
+
+
+
+
+
+#create a join
+select *
+from layoffs_staging2 table1
+join layoffs_staging2 table2
+	on table1.company = table2.company
+    and table1.location = table2.location
+where (table1.industry is null or table1.industry = '')
+and table2.industry is not null;
+
+update layoffs_staging2 table1
+join layoffs_staging2 table2
+	on table1.company = table2.company
+set table1.industry = table2.industry
+where (table1.industry is null or table1.industry = '')
+and table2.industry is not null;
+
+select *
+from layoffs_staging2;
+
+
+select *
+from layoffs_staging2
+where total_laid_off is null
+and percentage_laid_off is null;
+
+DELETE
+FROM layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL; #her fjerne vi støj fra datatabellen
+
+select *
+from layoffs_staging2;
+
+#nu fjerner vi row_num fordi det ikke er nødvendigt længere
+ALTER TABLE layoffs_staging2
+DROP COLUMN row_num;
